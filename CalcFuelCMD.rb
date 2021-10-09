b30e24c279fe7cac349c96fe2859937428fd1c1e
Maged <maged@thewebop.com>
2021-10-09 19:31:07 +0200

    variable purposes clearer #2

--- CalcFuelCMD.rb
+++ CalcFuelCMD.rb
@@ -2,13 +2,13 @@ require './CalcFuel'
 
 shipWeight = ARGV[0].tr(',', '').to_i
 tripsSequence = []
-ll = ''
+launchLand = ''
 
 ARGV[1..].each_with_index { |val,index|
     if (index.even?)
-        ll = val.to_sym
+        launchLand = val.to_sym
     else
-        tripSequence.push([ll, val.to_f])
+        tripSequence.push([launchLand, val.to_f])
     end
 }
 
